5207f33e92df35507a8a5c25dbdfdfdb798acadb
LYLENG1 <lylymg8@gmail.com>
2025-01-13 10:19:32 +0700

    fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,22 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ShopContaxt } from "../context/ShopContext";
-import {CartContext} from "../context/CartContext";
-import logo from '../assets/images/icons/logo-01.png';
-
+import { CartContext } from "../context/CartContext";
+import { FaShoppingCart, FaSearch, FaHeart } from "react-icons/fa"; // Importing icons
+import logo from "../assets/images/icons/logo-01.png";
 
 const Header = ({ toggleCart }) => {
   const { setShowSearch, navigate } = useContext(ShopContaxt);
   const { getCartCount } = useContext(CartContext);
+
   const handleSearchClick = () => {
     setShowSearch(true);
     navigate("/shop");
   };
+
   return (
     <header>
-      {/* Header desktop */}
       <div className="container-menu-desktop">
-        {/* Topbar */}
         <div className="top-bar">
           <div className="content-topbar flex-sb-m h-full container">
             <div className="left-top-bar">
@@ -45,13 +45,10 @@ const Header = ({ toggleCart }) => {
 
         <div className="wrap-menu-desktop">
           <nav className="limiter-menu-desktop container">
-            {/* Logo desktop */}
             <a href="#" className="logo">
-             <img src={logo} alt="IMG-LOGO" />
-</a>
+              <img src={logo} alt="IMG-LOGO" />
+            </a>
 
-
-            {/* Menu desktop */}
             <div className="menu-desktop">
               <ul className="main-menu">
                 <li className="active-menu">
@@ -80,149 +77,31 @@ const Header = ({ toggleCart }) => {
               </ul>
             </div>
 
-            {/* Icon header */}
             <div className="wrap-icon-header flex-w flex-r-m">
-              <div className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-                <i className="zmdi zmdi-search" onClick={handleSearchClick}></i>
+              <div className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11">
+                <FaSearch size={20} onClick={handleSearchClick} />
               </div>
+
               <Link to="/feature">
-              {/* <Link onClick={toggleCart}> */}
                 <div
                   className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
                   data-notify={getCartCount()}
                   onClick={toggleCart}
                 >
-                  <i className="zmdi zmdi-shopping-cart"></i>
-                  
+                  <FaShoppingCart size={20} />
                 </div>
               </Link>
 
               <a
                 href="#"
                 className="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti"
-                data-notify="10000"
+                data-notify="0"
               >
-                <i className="zmdi zmdi-favorite-outline"></i>
+                <FaHeart size={20} />
               </a>
             </div>
           </nav>
         </div>
-      </div>
-
-      {/* Header Mobile */}
-      <div className="wrap-header-mobile">
-        {/* Logo mobile */}
-        <div className="logo-mobile">
-          <a href="index.html">
-            <img src="images/icons/logo-01.png" alt="IMG-LOGO" />
-          </a>
-        </div>
-
-        {/* Icon header */}
-        <div className="wrap-icon-header flex-w flex-r-m m-r-15">
-          <div className="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
-            <i className="zmdi zmdi-search"></i>
-          </div>
-
-          <div
-            className="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart"
-            data-notify="2"
-          >
-            <i className="zmdi zmdi-shopping-cart"></i>
-          </div>
-
-          <a
-            href="#"
-            className="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti"
-            data-notify="0"
-          >
-            <i className="zmdi zmdi-favorite-outline"></i>
-          </a>
-        </div>
-
-        {/* Button show menu */}
-        <div className="btn-show-menu-mobile hamburger hamburger--squeeze">
-          <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
-          </span>
-        </div>
-      </div>
-
-      {/* Menu Mobile */}
-      <div className="menu-mobile">
-        <ul className="topbar-mobile">
-          <li>
-            <div className="left-top-bar">
-              Free shipping for standard order over $100
-            </div>
-          </li>
-
-          <li>
-            <div className="right-top-bar flex-w h-full">
-              <a href="#" className="flex-c-m p-lr-10 trans-04">
-                Help & FAQs
-              </a>
-
-              <a href="#" className="flex-c-m p-lr-10 trans-04">
-                My Account
-              </a>
-
-              <a href="#" className="flex-c-m p-lr-10 trans-04">
-                EN
-              </a>
-
-              <a href="#" className="flex-c-m p-lr-10 trans-04">
-                USD
-              </a>
-            </div>
-          </li>
-        </ul>
-
-        <ul className="main-menu-m">
-          <li>
-            <a href="index.html">Home</a>
-            <ul className="sub-menu-m">
-              <li>
-                <a href="index.html">Homepage 1</a>
-              </li>
-              <li>
-                <a href="home-02.html">Homepage 2</a>
-              </li>
-              <li>
-                <a href="home-03.html">Homepage 3</a>
-              </li>
-            </ul>
-            <span className="arrow-main-menu-m">
-              <i className="fa fa-angle-right" aria-hidden="true"></i>
-            </span>
-          </li>
-
-          <li>
-            <a href="product.html">Shop</a>
-          </li>
-
-          <li>
-            <a
-              href="shoping-cart.html"
-              className="label1 rs1"
-              data-label1="hot"
-            >
-              Features
-            </a>
-          </li>
-
-          <li>
-            <a href="blog.html">Blog</a>
-          </li>
-
-          <li>
-            <a href="about.html">About</a>
-          </li>
-
-          <li>
-            <a href="contact.html">Contact</a>
-          </li>
-        </ul>
       </div>
     </header>
   );
